ece72fca18c8117e800da6ee3d9ad00fc399d478
Laura Oprisan <isbasoiulaura@yahoo.com>
2023-02-10 19:30:21 +0200

    second try

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-//const PORT = 8000
+const PORT = 8000
 
 const rappers = {
     '21 savage': {
@@ -35,7 +35,7 @@ app.get('/api/:name', (request,response)=>{
     
 })
 
-// app.listen(PORT,()=>{
-//     console.log(`The server is running on port ${PORT}`)
-// })
+app.listen(PORT,()=>{
+    console.log(`The server is running on port ${PORT}`)
+})
 
